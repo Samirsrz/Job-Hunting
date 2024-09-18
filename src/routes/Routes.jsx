f39d3ec058edac7a92535e7201d29b2cfba07e4f
Samirsrz <shahilahamed2001@gmail.com>
2024-09-18 18:24:38 +0600

    daisyUI done

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,8 +1,21 @@
 import { createBrowserRouter } from 'react-router-dom'
+import Login from '../pages/Login/Login';
+import SignUp from '../pages/SignUp/SignUp';
 
 export const router = createBrowserRouter([
     {
       path: "/",
       element: <div>Hello world!</div>,
     },
+
+
+
+
+    { path: '/login', element: <Login /> },
+    { path: '/signup', element: <SignUp /> },
+
+
+
+
+
   ]);
