8f825fec25f7f289a87224b761710ac11339d3c2
Samirsrz <shahilahamed2001@gmail.com>
2024-09-19 00:08:49 +0600

    structured

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Navbar from "../components/navbar/Navbar";
 import Categories from './../Page/Categories';
+import SignUp from "../pages/SignUp/SignUp";
 
 export const router = createBrowserRouter([
   {
@@ -14,5 +15,9 @@ export const router = createBrowserRouter([
   {
     path: "/categories",
     element: <Categories></Categories>
+  },
+  {
+    path:'signup',
+    element: <SignUp/>
   }
 ]);
