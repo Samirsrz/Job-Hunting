b6d5ea82c607dcc1f6ba1c8234142bfe396dbba6
shaishabcoding <myshaishab@gmail.com>
2024-09-18 16:35:19 +0600

    Done Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,47 +1,79 @@
+import { NavLink } from "react-router-dom";
+
+const SearchBar = () => (
+  <button className="py-2 px-2 md:px-4 rounded-full bg-white/20 hover:bg-white/40 hover:scale-105 md:rounded-md font-semibold">
+    <span className="hidden md:inline md:mr-1">Search now</span>🔍
+  </button>
+);
+const links = [
+  {
+    title: "Home",
+    link: "/",
+  },
+  {
+    title: "Jobs",
+    link: "/jobs",
+  },
+  {
+    title: "Employers",
+    link: "/employers",
+  },
+  {
+    title: "About Us",
+    link: "/about",
+  },
+];
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100">
-      <div className="flex-1">
-        <a className="btn btn-ghost text-xl">Job Hunting</a>
-      </div>
-      <div className="flex-none gap-2">
-        <div className="form-control">
-          <input
-            type="text"
-            placeholder="Search"
-            className="input input-bordered w-24 md:w-auto"
-          />
-        </div>
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+    <div id="sidebar" className="navbar bg-primary text-white">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
               />
-            </div>
+            </svg>
           </div>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow text-black"
           >
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
-            </li>
+            {links.map(({ title, link }, idx) => (
+              <li key={idx}>
+                <NavLink to={link}>{title}</NavLink>
+              </li>
+            ))}
           </ul>
+        </div>
+        <a className="btn btn-ghost text-xl font-semibold">Job Hunting</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          {links.map(({ title, link }, idx) => (
+            <li key={idx}>
+              <NavLink to={link}>{title}</NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="navbar-end gap-4">
+        <div>
+          <SearchBar />
+        </div>
+        <div className="avatar">
+          <div className="ring-primary ring-offset-base-100 w-12 rounded-full ring ring-offset-2">
+            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+          </div>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from "daisyui";
 export default {
+  darkMode: "selector",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#60a5fa", // skyblue
+      },
+      fontFamily: {
+        "open-sans": '"Open Sans", sans-serif',
+      },
+    },
   },
   plugins: [daisyui],
 };
