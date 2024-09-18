e967f992c7a66088ae3867316d253e0f327d1540
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2024-09-18 20:27:14 +0600

    starts

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import Navbar from "../components/navbar/Navbar";
+import Home from "../components/HomePage/Home/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: (
       <>
-        <Navbar />
+        {/* <Navbar /> */}
+        <Home/>
       </>
     ),
   },
