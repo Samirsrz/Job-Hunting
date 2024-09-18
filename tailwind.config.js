5e03891733015d193247c8a208d52e605d2373b0
shaishabcoding <myshaishab@gmail.com>
2024-09-18 16:03:23 +0600

    (feat) added daisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [daisyui],
+};
