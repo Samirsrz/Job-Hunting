00b332b2fbea94aea57169b082d63c85822f7039
AbuRahat889 <aburahatshaum889@gmail.com>
2024-09-18 21:50:36 +0600

    edit: categories card edit

--- src/Page/Categories.jsx
+++ src/Page/Categories.jsx
@@ -4,109 +4,143 @@ import { Link } from "react-router-dom";
 import CategoriesCard from "../components/navbar/CategoriesCard";
 
 const Categories = () => {
+  //filter data by category..
+  const designs = datas.filter(
+    (design) => design.category == "Design/Creative"
+  );
+  const telecommunication = datas.filter(
+    (design) => design.category == "IT/Telecommunication"
+  );
+  const hospitality = datas.filter(
+    (design) => design.category == "Hospitality/Travel/Tourism"
+  );
   return (
-    <div className="max-w-screen-xl m-auto  mt-5">
-      <div className="mt-4">
-        <h1 className="text-2xl font-bold mb-4">Design/Creative</h1>
-        <Link>
-          <CategoriesCard img={img1} title={"web_development"}></CategoriesCard>
-        </Link>
+    <div className="max-w-screen-xl m-auto  mt-5 mb-20">
+      {/* Design/Creative */}
+      <div className="mt-10">
+        <h1 className="text-2xl font-bold mb-8">Design/Creative</h1>
+        <div className="flex gap-2 flex-wrap">
+          {telecommunication?.map((data) => (
+            <Link key={data.title}>
+              <CategoriesCard
+                img={data.image}
+                title={data.title}
+              ></CategoriesCard>
+            </Link>
+          ))}
+        </div>
       </div>
-      <div className="mt-4">
-        <h1 className="text-2xl font-bold mb-4">IT/Telecommunication</h1>
-        <Link>
-          <CategoriesCard img={img1} title={"web_development"}></CategoriesCard>
-        </Link>
+
+      {/* IT/Telecommunication */}
+      <div className="mt-10">
+        <h1 className="text-2xl font-bold mb-8">IT/Telecommunication</h1>
+        <div className="flex gap-2 flex-wrap">
+          {designs?.map((data) => (
+            <Link key={data.title}>
+              <CategoriesCard
+                img={data.image}
+                title={data.title}
+              ></CategoriesCard>
+            </Link>
+          ))}
+        </div>
       </div>
-      <div className="mt-4">
-        <h1 className="text-2xl font-bold mb-4">
-          Hospitality/ Travel/ Tourism
-        </h1>
-        <Link>
-          <CategoriesCard img={img1} title={"web_development"}></CategoriesCard>
-        </Link>
+
+      {/* hospitality */}
+      <div className="mt-10">
+        <h1 className="text-2xl font-bold mb-8">Hospitality/Travel/Tourism</h1>
+        <div className="flex gap-2 flex-wrap">
+          {hospitality?.map((data) => (
+            <Link key={data.title}>
+              <CategoriesCard
+                img={data.image}
+                title={data.title}
+              ></CategoriesCard>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
 };
 
-const data = [
+const datas = [
   {
-    "category": "Design/Creative",
-    "title": "Senior Graphic Designer",
-    "image": "https://example.com/images/graphic-designer.png"
+    category: "Design/Creative",
+    title: "Senior Graphic Designer",
+    image: "https://i.ibb.co.com/Lg5zT0r/undraw-Design-feedback-re-8gtk.png",
   },
   {
-    "category": "IT/Telecommunication",
-    "title": "Full Stack Web Developer",
-    "image": "https://example.com/images/web-developer.png"
+    category: "IT/Telecommunication",
+    title: "Full Stack Web Developer",
+    image: "https://i.ibb.co.com/Lg5zT0r/undraw-Design-feedback-re-8gtk.png",
   },
   {
-    "category": "Hospitality/Travel/Tourism",
-    "title": "Hotel Manager",
-    "image": "https://example.com/images/hotel-manager.png"
+    category: "Hospitality/Travel/Tourism",
+    title: "Hotel Manager",
+    image: "https://i.ibb.co.com/Lg5zT0r/undraw-Design-feedback-re-8gtk.png",
   },
   {
-    "category": "Design/Creative",
-    "title": "UI/UX Designer",
-    "image": "https://example.com/images/ui-ux-designer.png"
+    category: "Design/Creative",
+    title: "UI/UX Designer",
+    image:
+      "https://i.ibb.co.com/Lg5zT0r/undraw-Design-feedback-re-8gtk.pnghttps://i.ibb.co.com/TkrGLKK/undraw-Design-team-re-gh2d.png",
   },
   {
-    "category": "IT/Telecommunication",
-    "title": "Network Administrator",
-    "image": "https://example.com/images/network-admin.png"
+    category: "IT/Telecommunication",
+    title: "Network Administrator",
+    image: "https://i.ibb.co.com/h7HJMQ2/undraw-Art-lover-re-fn8g.png",
   },
   {
-    "category": "Hospitality/Travel/Tourism",
-    "title": "Travel Consultant",
-    "image": "https://example.com/images/travel-consultant.png"
+    category: "Hospitality/Travel/Tourism",
+    title: "Travel Consultant",
+    image: "https://i.ibb.co.com/h7HJMQ2/undraw-Art-lover-re-fn8g.png",
   },
   {
-    "category": "Design/Creative",
-    "title": "Art Director",
-    "image": "https://example.com/images/art-director.png"
+    category: "Design/Creative",
+    title: "Art Director",
+    image: "https://i.ibb.co.com/h7HJMQ2/undraw-Art-lover-re-fn8g.png",
   },
   {
-    "category": "IT/Telecommunication",
-    "title": "Cybersecurity Analyst",
-    "image": "https://example.com/images/cybersecurity-analyst.png"
+    category: "IT/Telecommunication",
+    title: "Cybersecurity Analyst",
+    image: "https://i.ibb.co.com/4s0pTCP/undraw-Interior-design-re-7mvn.png",
   },
   {
-    "category": "Hospitality/Travel/Tourism",
-    "title": "Tour Guide",
-    "image": "https://example.com/images/tour-guide.png"
+    category: "Hospitality/Travel/Tourism",
+    title: "Tour Guide",
+    image: "https://i.ibb.co.com/4s0pTCP/undraw-Interior-design-re-7mvn.png",
   },
   {
-    "category": "Design/Creative",
-    "title": "Interior Designer",
-    "image": "https://example.com/images/interior-designer.png"
+    category: "Design/Creative",
+    title: "Interior Designer",
+    image: "https://i.ibb.co.com/4s0pTCP/undraw-Interior-design-re-7mvn.png",
   },
   {
-    "category": "IT/Telecommunication",
-    "title": "Software Engineer",
-    "image": "https://example.com/images/software-engineer.png"
+    category: "IT/Telecommunication",
+    title: "Software Engineer",
+    image: "https://i.ibb.co.com/4s0pTCP/undraw-Interior-design-re-7mvn.png",
   },
   {
-    "category": "Hospitality/Travel/Tourism",
-    "title": "Flight Attendant",
-    "image": "https://example.com/images/flight-attendant.png"
+    category: "Hospitality/Travel/Tourism",
+    title: "Flight Attendant",
+    image: "https://i.ibb.co.com/4s0pTCP/undraw-Interior-design-re-7mvn.png",
   },
   {
-    "category": "Design/Creative",
-    "title": "Fashion Designer",
-    "image": "https://example.com/images/fashion-designer.png"
+    category: "Design/Creative",
+    title: "Fashion Designer",
+    image: "https://i.ibb.co.com/bv5tfW9/undraw-fashion-photoshoot-mtq8.png",
   },
   {
-    "category": "IT/Telecommunication",
-    "title": "DevOps Engineer",
-    "image": "https://example.com/images/devops-engineer.png"
+    category: "IT/Telecommunication",
+    title: "DevOps Engineer",
+    image: "https://i.ibb.co.com/4s0pTCP/undraw-Interior-design-re-7mvn.png",
   },
   {
-    "category": "Hospitality/Travel/Tourism",
-    "title": "Event Planner",
-    "image": "https://example.com/images/event-planner.png"
-  }
-]
-
+    category: "Hospitality/Travel/Tourism",
+    title: "Event Planner",
+    image: "https://i.ibb.co.com/4s0pTCP/undraw-Interior-design-re-7mvn.png",
+  },
+];
 
 export default Categories;
--- src/components/navbar/CategoriesCard.jsx
+++ src/components/navbar/CategoriesCard.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const CategoriesCard = ({ img, title }) => {
   return (
     <div>
-      <div className="card bg-base-100 w-64 shadow-xl border border-primary">
+      <div className="card bg-base-100 w-64 h-60 shadow-xl border border-primary hover:scale-105 transform hover:ease-in duration-300">
         <figure>
-          <img className="w-96 text-primary" src={img} alt="Shoes" />
+          <img className="size-full text-primary" src={img} alt="Shoes" />
         </figure>
         <div className="card-body">
           <h2 className="card-title">{title}</h2>
