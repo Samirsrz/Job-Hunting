142d3ff5367de63610b205b8c4f33abc4d450ad1
Samirsrz <shahilahamed2001@gmail.com>
2024-09-19 16:06:54 +0600

    done with signUp

---
+++ src/api/utils/index.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export const imageUpload = async (image) => {
+    const formData = new FormData();
+    formData.append('image', image)
+    const {data} = await axios.post(`https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB_API_KEY}`, formData )
+    const photo = data.data.display_url;
+
+    return photo
+}
