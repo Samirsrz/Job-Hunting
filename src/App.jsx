fed1ca44a388d70e1fa1fb6b923ba8971cded701
AbuRahat889 <aburahatshaum889@gmail.com>
2024-09-20 16:29:44 +0600

    create footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/navbar/Footer";
+import Footer from "./components/Footer/Footer";
+
 
 function App() {
   return (
