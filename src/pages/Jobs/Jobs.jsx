a73f8be006603e10fad984a9a62a2d786265d485
shaishabcoding <myshaishab@gmail.com>
2024-09-23 17:23:09 +0600

    added jobs api

--- src/pages/Jobs/Jobs.jsx
+++ src/pages/Jobs/Jobs.jsx
@@ -1,11 +1,19 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import JobCard from "../../components/jobs/JobCard";
 import { jobCategories } from "./bin/jobCategories";
-import { jobData } from "./bin/jobData";
+
 const Jobs = () => {
+  const [jobs, setJobs] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/jobs")
+      .then((data) => setJobs(data.data.data));
+  }, []);
   return (
     <div>
       <h1 className="text-center text-5xl font-bold mt-10 mb-6">
-        Jobs ({jobData?.length})
+        Jobs ({jobs?.length})
       </h1>
       <div className="mx-6">
         <h2 className="text-lg font-semibold my-2">Categories</h2>
@@ -18,7 +26,7 @@ const Jobs = () => {
         </div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 justify-between gap-6 m-6">
-        {jobData?.map((job, idx) => (
+        {jobs?.map((job, idx) => (
           <JobCard {...{ job }} key={idx} />
         ))}
       </div>
