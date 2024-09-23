d37c75b7763e035e70b33376de7ffa8c65635680
shaishabcoding <myshaishab@gmail.com>
2024-09-23 17:36:18 +0600

    added category

--- src/pages/Jobs/Jobs.jsx
+++ src/pages/Jobs/Jobs.jsx
@@ -1,15 +1,22 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import JobCard from "../../components/jobs/JobCard";
-import { jobCategories } from "./bin/jobCategories";
 
 const Jobs = () => {
   const [jobs, setJobs] = useState([]);
+  const [categories, setCategories] = useState([]);
   useEffect(() => {
     axios
       .get("http://localhost:8000/jobs")
       .then((data) => setJobs(data.data.data));
   }, []);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/category")
+      .then((data) => setCategories(data.data.data));
+  }, []);
+
   return (
     <div>
       <h1 className="text-center text-5xl font-bold mt-10 mb-6">
@@ -18,7 +25,8 @@ const Jobs = () => {
       <div className="mx-6">
         <h2 className="text-lg font-semibold my-2">Categories</h2>
         <div className="flex flex-row flex-wrap gap-2">
-          {jobCategories?.map((cat, idx) => (
+          <button className="btn btn-sm">All</button>
+          {categories?.map((cat, idx) => (
             <button className="btn btn-sm" key={idx}>
               {cat}
             </button>
