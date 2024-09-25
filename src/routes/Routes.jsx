6a63292fdd72ee88e7af965dd07d7a1b06daada4
AbuRahat889 <aburahatshaum889@gmail.com>
2024-09-25 22:02:44 +0600

    fiexd issue

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -7,20 +7,16 @@ import Home from "../pages/HomePage/Home";
 import SignUp from "../pages/SignUp/SignUp";
 import Jobs from "../pages/Jobs/Jobs";
 
-import Login from "../pages/Login/Login";
 import ErrorPage from "../components/ErrorPage/ErrorPage";
 import DashboardLayout from "../DashboardLayout/DashboardLayout";
 import ViewsJob from "../pages/UserDashboard/ViewsJob";
 import AppliedJobs from "../pages/UserDashboard/AppliedJobs";
-<<<<<<< HEAD
 import Statictis from "../components/Dashboard/Statictis";
 import UserProfile from "../pages/UserDashboard/UserProfile";
 import AllUser from "../pages/AdminDashboard/AllUser";
+import JobDetails from './../pages/Jobs/JobDetails';
 
 
-=======
-import JobDetails from "../pages/Jobs/JobDetails";
->>>>>>> 9784d01cf3b129b6f83ca5851a33c8ec10fdc038
 
 export const router = createBrowserRouter([
   {
