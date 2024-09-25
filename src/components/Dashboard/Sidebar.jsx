f31eae80e999f057210b407815e70e3ab7562374
Samirsrz <shahilahamed2001@gmail.com>
2024-09-25 17:48:50 +0600

    added google login with role

--- src/components/Dashboard/Sidebar.jsx
+++ src/components/Dashboard/Sidebar.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import { GrLogout } from 'react-icons/gr'
 import { FcSettings } from 'react-icons/fc'
-import { BsFingerprint, BsFillHouseAddFill } from 'react-icons/bs'
-import { GrUserAdmin } from 'react-icons/gr'
+import { BsFillHouseAddFill } from 'react-icons/bs'
+
 import { MdHomeWork } from 'react-icons/md'
 import { AiOutlineBars } from 'react-icons/ai'
 import { BsGraphUp } from 'react-icons/bs'
