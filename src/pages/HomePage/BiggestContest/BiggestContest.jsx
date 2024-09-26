05af12c433e09daf0c16a118f52842683c526f1b
Tamanna Chadni <tamanna.chadni.eng@gmail.com>
2024-09-27 02:10:12 +0600

    Added language feature to the project

--- src/pages/HomePage/BiggestContest/BiggestContest.jsx
+++ src/pages/HomePage/BiggestContest/BiggestContest.jsx
@@ -3,9 +3,11 @@
 
 import React from 'react';
 import './BiggestContest.css'; // Create a separate CSS file for the animation styles
-import companyContest from '../../../../src/assets/company/838020_file.svg'
+import companyContest from '../../../../src/assets/company/838020_file.svg';
+import { useTranslation } from 'react-i18next';
 
 const BiggestContest = () => {
+  const { t } = useTranslation();
   return (
     <section className="bg-[#5900f4f9] mt-12 relative flex justify-between flex-col lg:flex-row gap-3 items-center lg:h-[200px] rounded-3xl">
       <article className="flex justify-between items-center lg:justify-normal lg:gap-10 bg-[#00000055] px-14 w-full lg:w-[300px] rounded-r-3xl  lg:rounded-r-[70px] rounded-l-3xl overflow-hidden">
@@ -65,12 +67,12 @@ const BiggestContest = () => {
       </article>
 
       <article className='text-center lg:text-left '  >
-        <h1 className='font-bold text-3xl text-white px-3 lg:p-0'>Bangladesh &rsquo;s  biggest skill contest <br/> to help you stand out to reqruiters</h1>
+        <h1 className='font-bold text-3xl text-white px-3 lg:p-0'>{t('Bangladesh&rsquo;sbiggestskillcontest')}  <br/> {t('tohelpyoustandouttoreqruiters')}</h1>
         <button className='border border-white rounded-3xl px-4 mt-3 py-2 bg-[#5900f454] text-white font-semibold'>win from a prize pool of 2 lac Tk</button>
         <button className='border border-white rounded-3xl px-4 mt-3 py-2 bg-[#5900f454] text-white font-semibold'> Boost with a winner&#39;s certificate</button>
       </article>
       <article className= 'my-3 lg:my-0'>
-        <button className='font-bold text-3xl bg-yellow-400 rounded-3xl px-4 py-2 mr-3'>Tell me more</button>
+        <button className='font-bold text-3xl bg-yellow-400 rounded-3xl px-4 py-2 mr-3'>{t('Tellmemore')}</button>
       </article>
     </section>
   );
