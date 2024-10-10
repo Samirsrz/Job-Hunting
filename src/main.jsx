34ec36919c47e50d7b073f9ed23fc919803d77dd
AbuRahat889 <aburahatshaum889@gmail.com>
2024-10-10 11:40:59 +0600

    solved conflict

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./i18n.js";
 // import "swiper/css";
-import "swiper/css/effect-cards";
+// import "swiper/css/effect-cards";
 
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/Routes.jsx";
