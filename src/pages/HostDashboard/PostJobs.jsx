f092b962765e2b4de2d29d4a6f90271c473cc9bb
Samirsrz <shahilahamed2001@gmail.com>
2024-10-12 03:41:32 +0600

    UI design for payment

--- src/pages/HostDashboard/PostJobs.jsx
+++ src/pages/HostDashboard/PostJobs.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
 import { imageUpload } from "../../api/utils";
 import toast from "react-hot-toast";
+import { Link } from "react-router-dom";
 
 
 const PostJobs = () => {
@@ -224,6 +225,34 @@ const PostJobs = () => {
               </div>
             </div>
           </div>
+
+             <Link to='/dashboard/payment'>
+             <div className="flex justify-center space-x-6 py-8">
+
+  <div className="card bg-gray-100 rounded-lg shadow-lg p-8 text-center w-1/3">
+    <h2 className="text-2xl font-bold mb-4">Regular Membership</h2>
+    <p className="text-gray-700 mb-6">Basic job posting with standard visibility.</p>
+    <p className="text-xl font-semibold mb-6">$10 per/month</p>
+    <p className="bg-blue-500 text-white font-semibold py-2 px-6 rounded hover:bg-blue-600 transition block">
+      Select Regular
+    </p>
+  </div>
+
+
+  <div className="card bg-yellow-100 rounded-lg shadow-lg p-8 text-center w-1/3">
+    <h2 className="text-2xl font-bold mb-4">Premium Membership</h2>
+    <p className="text-gray-700 mb-6">Featured job posting with high visibility and priority listing.</p>
+    <p className="text-xl font-semibold mb-6">$100 per/year</p>
+    <p className="bg-green-500 text-white font-semibold py-2 px-6 rounded hover:bg-green-600 transition block">
+      Select Premium
+    </p>
+  </div>
+</div>
+
+</Link>
+
+
+
 
           <button
             disabled={loading}
---
+++ src/pages/Payment/Payment.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Payment = () => {
+    return (
+        <div>
+            THis is Payment
+        </div>
+    );
+};
+
+export default Payment;
