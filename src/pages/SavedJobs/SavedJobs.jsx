6747f5288f1e902c84180089e73c409fd94d6cf6
shaishabcoding <myshaishab@gmail.com>
2024-10-21 21:03:15 +0600

    added no data lottie

--- src/pages/SavedJobs/SavedJobs.jsx
+++ src/pages/SavedJobs/SavedJobs.jsx
@@ -3,10 +3,12 @@ import JobCard from "../../components/jobs/JobCard";
 import { localGetJobs } from "../../libs/localJobs";
 import { useGetJobsByIdsQuery } from "../../RTK/features/jobsApi";
 import { useSavedJobs } from "../../providers/SavedJobsContext";
+import Lottie from "lottie-react";
+import noData from "../../../public/Annimations/no-data.json";
 
 const SavedJobs = () => {
   const { savedJobs } = useSavedJobs();
-  const { data, error, isLoading } = useGetJobsByIdsQuery(savedJobs, {
+  const { data, error, isLoading, refetch } = useGetJobsByIdsQuery(savedJobs, {
     skip: savedJobs.length === 0,
   });
 
@@ -14,12 +16,19 @@ const SavedJobs = () => {
   if (error) return <p>Error loading jobs: {error.message}</p>;
 
   if (!savedJobs?.length) {
-    return <p>No saved jobs found.</p>;
+    return (
+      <div className="flex items-center justify-center flex-col h-screen">
+        <h2 className="text-3xl font-semibold">No saved job found!</h2>
+        <Lottie animationData={noData} className="h-96 w-72 lg:w-96"></Lottie>
+      </div>
+    );
   }
 
   return (
     <div>
-      <h1>Saved Jobs</h1>
+      <h1 className="text-center md:font-semibold font-medium md:text-4xl text-2xl lg:my-20 md:my-10 my-6">
+        Saved Jobs
+      </h1>
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 justify-between gap-6 m-6">
         {data.data.map((job, idx) => (
           <JobCard
