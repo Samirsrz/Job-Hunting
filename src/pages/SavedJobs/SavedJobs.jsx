a28f868b7ed77c275070467c7061c21dea0ea377
shaishabcoding <myshaishab@gmail.com>
2024-10-21 21:22:08 +0600

    added error and loading lottie

--- src/pages/SavedJobs/SavedJobs.jsx
+++ src/pages/SavedJobs/SavedJobs.jsx
@@ -4,7 +4,10 @@ import { localGetJobs } from "../../libs/localJobs";
 import { useGetJobsByIdsQuery } from "../../RTK/features/jobsApi";
 import { useSavedJobs } from "../../providers/SavedJobsContext";
 import Lottie from "lottie-react";
+import { RxReload } from "react-icons/rx";
 import noData from "../../../public/Annimations/no-data.json";
+import errorData from "../../../public/Annimations/error.json";
+import loadingData from "../../../public/Annimations/loading.json";
 
 const SavedJobs = () => {
   const { savedJobs } = useSavedJobs();
@@ -12,14 +15,36 @@ const SavedJobs = () => {
     skip: savedJobs.length === 0,
   });
 
-  if (isLoading) return <p>Loading saved jobs...</p>;
-  if (error) return <p>Error loading jobs: {error.message}</p>;
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center flex-col py-20">
+        <Lottie
+          animationData={loadingData}
+          className="h-72 w-72 lg:w-96 my-10"
+        ></Lottie>
+      </div>
+    );
+  }
+  if (error) {
+    return (
+      <div className="flex items-center justify-center flex-col py-20">
+        <h2 className="text-3xl font-semibold">Something went wrong!</h2>
+        <Lottie
+          animationData={errorData}
+          className="h-44 w-44 lg:w-96 my-10"
+        ></Lottie>
+        <button onClick={refetch} className="btn btn-error">
+          Try Again <RxReload className="inline" />
+        </button>
+      </div>
+    );
+  }
 
   if (!savedJobs?.length) {
     return (
-      <div className="flex items-center justify-center flex-col h-screen">
+      <div className="flex items-center justify-center flex-col py-20">
         <h2 className="text-3xl font-semibold">No saved job found!</h2>
-        <Lottie animationData={noData} className="h-96 w-72 lg:w-96"></Lottie>
+        <Lottie animationData={noData} className="h-72 w-72 lg:w-96"></Lottie>
       </div>
     );
   }
