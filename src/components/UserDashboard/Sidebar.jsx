c5f35603468582ba2621795fb7e3987200f252fe
Tamanna Chadni <tamanna.chadni.eng@gmail.com>
2024-10-24 14:21:28 +0600

    Resume builder feature to convert Pdf format

--- src/components/UserDashboard/Sidebar.jsx
+++ src/components/UserDashboard/Sidebar.jsx
@@ -189,6 +189,25 @@ const Sidebar = () => {
 
                     <span className="mx-4 font-medium">Interview schedule</span>
                   </NavLink>
+                  {/* Interview schedule */}
+                  {/*  resume builder*/}
+                  <NavLink
+                    to="/dashboard/resume-builder"
+                    className={({ isActive }) =>
+                      `flex items-center px-4 py-2 my-5  transition-colors duration-300 transform  hover:bg-gray-300   hover:text-gray-700 ${
+                        isActive
+                          ? "bg-gray-300  text-gray-700"
+                          : "text-gray-600"
+                      }`
+                    }
+                  >
+                    <MdHomeWork className="w-5 h-5" />
+
+                    <span className="mx-4 font-medium">Create Resume</span>
+                  </NavLink>
+
+                  {/*  resume builder*/}
+
                 </div>
               )}
 
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -32,6 +32,7 @@ import UserStatictis from "../pages/UserDashboard/UserStatictis";
 import ViewHostJobs from "../pages/HostDashboard/ViewHostJobs";
 import ViewAllJobs from "../pages/AdminDashboard/ViewAllJobs";
 import HostStatisticPage from "../pages/HostDashboard/HostStatisticPage";
+import ResumeBuilder from "../pages/ResumeBuilder/ResumeBuilder";
 
 export const router = createBrowserRouter([
   {
@@ -127,6 +128,10 @@ export const router = createBrowserRouter([
       {
         path: "/dashboard/interview-schedule",
         element: <InterviewSchedule />,
+      },
+      {
+        path: "/dashboard/resume-builder",
+        element: <ResumeBuilder></ResumeBuilder>,
       },
       {
         path: "/dashboard/userprofile",
